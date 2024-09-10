d565a86385372de277bb1681b3f4b424e7c2afe1
Jeremy Fowler <jembyfowler@gmail.com>
2024-09-10 09:57:21 -0600

    set up schema

---
+++ src/models/Car.js
@@ -0,0 +1,22 @@
+import { Schema } from "mongoose";
+
+export const carSchema = new Schema(
+  {
+    make: { type: String, minlength: 3, maxlength: 50, required: true },
+    model: { type: String, minlength: 1, maxlength: 100, required: true },
+    year: { type: Number, min: 1886, max: 2025, required: true },
+    price: { type: Number, min: 0, max: 1000000, required: true },
+    imgUrl: { type: String, minlength: 0, maxlength: 500, required: true },
+    description: { type: String, minlength: 0, maxlength: 500, required: false },
+    // NOTE enum denotes that the property must be one of the values in the array
+    engineType: { type: String, enum: ['V6', 'V8', 'V10', '4-cylinder', 'unknown'], default: 'unknown', required: true },
+    color: { type: String, minlength: 0, maxlength: 50, required: true },
+    // NOTE tags is an array of strings, and each string has a maxlength of 50
+    tags: [{ type: String, maxlength: 50 }],
+    mileage: { type: Number, min: 0, max: 1000000, required: true },
+    hasCleanTitle: { type: Boolean, required: true, default: true },
+    // NOTE the creatorId must be an objectId type (24 characters, specfic spots are numbers and letters)
+    // NOTE each creatorId should match the _id of an account in our accounts collection
+    creatorId: { type: Schema.ObjectId, required: true, ref: 'Account' }
+  }
+)
