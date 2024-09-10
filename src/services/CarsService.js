5830893e72b03f1dff01a94319b5839da2e08649
Jeremy Fowler <jembyfowler@gmail.com>
2024-09-10 12:13:22 -0600

    pagination

--- src/services/CarsService.js
+++ src/services/CarsService.js
@@ -17,8 +17,24 @@ class CarsService {
     const sortBy = query.sort
     delete query.sort
 
-    const cars = await dbContext.Cars.find(query).sort(sortBy + ' createdAt')
-    return cars
+    const pageNumber = parseInt(query.page) || 1
+    const limitAmount = 10
+    const skipAmount = (pageNumber - 1) * limitAmount
+    delete query.page
+
+    const cars = await dbContext.Cars.find(query)
+      .sort(sortBy + ' createdAt')
+      .skip(skipAmount)
+      .limit(limitAmount)
+
+    const carCount = await dbContext.Cars.countDocuments()
+
+    return {
+      results: cars,
+      count: carCount,
+      currentPage: pageNumber,
+      totalPages: Math.ceil(carCount / limitAmount)
+    }
   }
 }
 
